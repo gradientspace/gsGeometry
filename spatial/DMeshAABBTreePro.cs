36c8f21040edff11dbd42c22cc78ec78bf42a92d
Ryan Schmidt <rms@rms80.com>
2018-11-22 11:40:06 -0500

    initial commit

---
+++ spatial/DMeshAABBTreePro.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Ryan Schmidt (rms@gradientspace.com) - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited. Proprietary and confidential.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using g3;
+
+namespace gs
+{
+
+    /// <summary>
+    /// Additions to base DMeshAABBTree3. Currently adds:
+    ///    - Fast Mesh Winding Number evaluation
+    /// </summary>
+    public class DMeshAABBTreePro : DMeshAABBTree3
+    {
+        public DMeshAABBTreePro(DMesh3 m, bool autoBuild = false) : base(m,autoBuild)
+        {
+        }
+
+    }
+}
